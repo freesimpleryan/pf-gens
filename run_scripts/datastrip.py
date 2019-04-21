0493db98464724748ccf1404d53fcf843e447727
rmbrowndigital <ryanbrown.digital@gmail.com>
2019-04-20 21:36:36 -0400

    Added more json

--- run_scripts/datastrip.py
+++ run_scripts/datastrip.py
@@ -6,44 +6,43 @@ def main():
     # Honestly don't try to get this to work - this is super rigged up
     json_data = {}
     pdf_path = ""
-    data_name = "npcbgs"
-    json_output_path = "../data/%s.json" % data_name
-    special_dash_char = "Œ"
-    MAX_VALUE = 100
-    page = 94
-    ranged = True
+    page = 99
     pdfFileObject = open(pdf_path, 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObject)
     p = pdfReader.getPage(page)
     page_text = p.extractText()
-    if ranged:
-        ranged_value_regex = "\d{1,2}Œ\d{1,3}"
-        raw_values = re.findall(ranged_value_regex, page_text)
-        for v in raw_values:
-            roll_range = v.split(special_dash_char)
-            text_start = page_text.split(v)[1]
-            text_end = text_start.split(special_dash_char)[0]
-            text_list = text_end.splitlines()
-            if text_list[-1].isdigit():
-                del text_list[-1]
-            text = ''.join(text_list)
-            text = text.replace('™', "'")
-            for value in roll_range:
-                json_data[value] = text
-    else:
-        for i in range(1,MAX_VALUE+1):
-            value = "%s" % str(i)
-            end_value = "%s" % str(i + 1)
-            start_text = page_text.split(value)[1]
-            captured_text = start_text.split(end_value)[0]
-            t = captured_text.splitlines()
-            text = ''.join(t)
-            json_data[value] = text
-    json_text = json.dumps(json_data)
-    with open(json_output_path, "w+") as json_file:
-        json_file.write(json_text)
-    print("Created %s" % json_output_path)
-    
+    ranged_value_regex = "\d{1,2}Œ\d{1,3}"
+    raw_values = re.findall(ranged_value_regex, page_text)
+    data = {
+        "data": {
+            "1": [],
+            "2": [],
+            "3": [],
+            "4": [],
+            "5": []
+        }
+    }
+    for index, r in enumerate(raw_values):
+        split_start = page_text.split(r)
+        # Get everything after break
+        start_capture = split_start[1]
+        if r == "99Œ100":
+            end_capture = start_capture
+        else:
+            # Get everything before break
+            split_end = start_capture.split(raw_values[index+1])
+            end_capture = split_end[0]
+        linebroken = end_capture.splitlines()
+        del linebroken[0]
+        if len(linebroken) > 5:
+            del linebroken[0]
+        for index, word in enumerate(linebroken):
+            d = data["data"]
+            d[str(index + 1)].append(word.strip())
+    with open("../data/npcgroupnames.json", "w+") as outfile:
+        outfile.write(json.dumps(data))
+    """with open("outtext.txt", "w+", encoding='utf-8') as txtfile:
+        txtfile.write(page_text)"""
 
 if __name__ == "__main__":
     main()
